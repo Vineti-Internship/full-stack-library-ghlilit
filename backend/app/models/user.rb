44f2dbbebd7f7fff30dc797f66d0c175bdf2caf5
Lilit Ghandilyan <lilit_ghandilyan@edu.aua.am>
2018-11-27 01:01:10 +0400

    Add validations and associations

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+        has_many :books,  dependent: :destroy
+        before_save { self.email = email.downcase }
+        VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+        validates :name, presence: true , length: {maximum: 50}
+        validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
 end
